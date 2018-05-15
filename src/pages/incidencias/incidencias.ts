31bfeb2a867d9adb0ded8611beb894f8c79b8476
inés <icarazo@cic.es>
2018-05-15 13:23:53 +0200

    Cambios en las incidencias

--- src/pages/incidencias/incidencias.ts
+++ src/pages/incidencias/incidencias.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @IonicPage()
 @Component({
@@ -11,11 +13,49 @@ export class IncidenciasPage {
   @ViewChild('email') mail;
   @ViewChild('incidencia') incidencia;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  incidenciaRef: AngularFireList<any>;
+  inci: Observable<any[]>;
+  correo: string;
+  texto: string;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public database: AngularFireDatabase
+  ) {
+    this.incidenciaRef = this.database.list('inciden');
+    this.inci = this.incidenciaRef.snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IncidenciasPage');
+  }
+
+  anadirIncidencia(){
+
+    this.correo = this.mail.val;
+    console.log('correo ', this.correo);
+
+   this.texto = this.incidencia.val;
+    console.log('texto ', this.texto);
+
+    data => {
+      data.incidenciaRef.push({
+        correo: data.correo,
+      incidencia: data.texto
+      })
+    }
+    
+   /* data => {
+      this.incidenciaRef.push({
+        incidencia data.title,
+        done: false
+        
+      });
+    }*/
   }
 
 }
